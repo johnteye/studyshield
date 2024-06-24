dc9cc8b48d8f0718cb696781c3ec3ef404fcdbfd
John Teye Doku <86769906+johnteye@users.noreply.github.com>
2024-06-24 09:09:46 +0000

    Initialize StudyShield
    First Commit to StudyShield

---
+++ src/index.jsx
@@ -0,0 +1,10 @@
+import * as React from 'react';
+import { createRoot } from 'react-dom/client';
+import Home from './components/Home.jsx';
+
+const root = createRoot(document.body);
+root.render(
+<>
+<Home />
+</>  
+);
